ee42536b28fa5fadbe0e02900252bbb8d6903314
kerimabdy <kerim060620@gmail.com>
2024-02-24 16:52:38 +0800

    add key to map

--- src/page/group/ui/hero.tsx
+++ src/page/group/ui/hero.tsx
@@ -17,27 +17,25 @@ export const StudentsGroupPageHero = (props: StudentGroupQuery) => {
 
 
 
-    {props.studentGroup.videos && props.studentGroup.videos.map(video => {
-      return <>
-        <div data-tina-field={video && tinaField(video)}>
-          <div className=" w-full border-t border-zinc-800 px-8 ">
-            <div className="relative w-full mx-auto max-w-screen-md border-x border-zinc-800 p-6">
-              <div className="flex flex-wrap gap-4 items-baseline">
-                <h2 className='text-6xl leading-none '>{video?.name}</h2>
-                <span className='text-zinc-500 text-xl'>({video?.name})</span>
-              </div>
-            </div>
-          </div>
-
-          <div className="border-t border-zinc-800">
-            <div className="relative w-full mx-auto max-w-screen-md border-x border-zinc-800 p-6">
-              <div className="bg-zinc-900 overflow-hidden relative rounded-xl w-full aspect-video">
-                <video muted playsInline controls src={video?.video || ""} className="object-fit absolute w-full h-full top-0 left-0 right-0 bottom-0"></video>
-              </div>
+    {props.studentGroup.videos && props.studentGroup.videos.map((video, index) => {
+      return <div key={`${video?.name} ${index}`} data-tina-field={video && tinaField(video)}>
+        <div className=" w-full border-t border-zinc-800 px-8 ">
+          <div className="relative w-full mx-auto max-w-screen-md border-x border-zinc-800 p-6">
+            <div className="flex flex-wrap gap-4 items-baseline">
+              <h2 className='text-6xl leading-none '>{video?.name}</h2>
+              <span className='text-zinc-500 text-xl'>({video?.name})</span>
             </div>
           </div>
         </div>
-      </>
+
+        <div className="border-t border-zinc-800">
+          <div className="relative w-full mx-auto max-w-screen-md border-x border-zinc-800 p-6">
+            <div className="bg-zinc-900 overflow-hidden relative rounded-xl w-full aspect-video">
+              <video muted playsInline controls src={video?.video || ""} className="object-fit absolute w-full h-full top-0 left-0 right-0 bottom-0"></video>
+            </div>
+          </div>
+        </div>
+      </div>
     })}
   </div>
 }
