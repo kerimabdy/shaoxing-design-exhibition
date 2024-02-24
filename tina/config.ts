519a3920f17ff23e45bc32d02deb5e0efd6a1d40
kerimabdy <kerim060620@gmail.com>
2024-02-24 16:52:49 +0800

    add cloudinary

--- tina/config.ts
+++ tina/config.ts
@@ -24,9 +24,13 @@ export default defineConfig({
     publicFolder: "public",
   },
   media: {
-    tina: {
-      mediaRoot: "uploads",
-      publicFolder: "public",
+    // tina: {
+    //   mediaRoot: "uploads",
+    //   publicFolder: "public",
+    // },
+    loadCustomStore: async () => {
+      const pack = await import("next-tinacms-cloudinary");
+      return pack.TinaCloudCloudinaryMediaStore;
     },
   },
   // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/schema/
